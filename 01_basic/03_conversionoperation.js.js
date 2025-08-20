fd4f430f6b53452228c7e16dd94e18fa29e7f529
Deepak7900 <147752684+Deepak7900@users.noreply.github.com>
2025-08-20 09:50:23 +0000

    conversion.js complete

--- 01_basic/03_conversionoperation.js.js
+++ 01_basic/03_conversionoperation.js.js
@@ -1,47 +1,97 @@
-let score = "deepak"
+// let score = "deepak"
 
 
-  console.log(typeof score);
+//   console.log(typeof score);
 
   
   
- let valueInNumber = Number(score)
-  console.log(typeof valueInNumber );
+//  let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber );
 
-  console.log(valueInNumber);
-
-
-
-  // "33"  == 33
-  // "33dh"  =  Nan 
-  // true  = 1
-  // false = 0
-  // undefined =  Nan
+//   console.log(valueInNumber);
 
 
-  let isLoggedIn = "hwv"
 
-  let deepak = Boolean(isLoggedIn)
-  console.log(deepak);
+//   // "33"  == 33
+//   // "33dh"  =  Nan 
+//   // true  = 1
+//   // false = 0
+//   // undefined =  Nan
+
+
+//   let isLoggedIn = "hwv"
+
+//   let deepak = Boolean(isLoggedIn)
+//   console.log(deepak);
   
 
    
   
-// hietsh = true  
-// null = flase 
-//
+// // hietsh = true  
+// // null = flase 
+// //
 
 
-let number = 33
+// let number = 33
 
-let stringnumber = String(number)
-console.log(stringnumber)
-console.log(typeof number);
-
-
-
-let onlystring = "ravi"
+// let stringnumber = String(number)
+// console.log(stringnumber)
+// console.log(typeof number);
 
 
-let onystring = Number()
+
+// let onlystring = "ravi"
+
+
+// let onystring = Number()
+
+
+
+//  *********************** operation*****************//
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+
+let str1 = "hello"
+let str2 = " deepak"
+
+let str3 = str1+str2
+
+
+console.log(str3);
+
+
+// console.log("1" + 2);  //  
+// console.log("1" +2+2);
+
+console.log(1+2+"3") ; // this will give  33 
+
+let num1,num2,num3
+
+num1=num2=num3= 2+2 
+
+
+let deepakcounter = 100
+++deepakcounter;
+console.log(deepakcounter);
+
+
+
+
+
+ 
+
+
+
+
 
