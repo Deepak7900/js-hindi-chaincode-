41ff2d1860f1bd5ef1b950835851b0cca0e464cc
Deepak7900 <147752684+Deepak7900@users.noreply.github.com>
2025-08-20 07:46:03 +0000

    conversin of data types

--- 01_basic/03_conversionoperation.js.js
+++ 01_basic/03_conversionoperation.js.js
@@ -1,4 +1,4 @@
-let score = 33
+let score = "deepak"
 
 
   console.log(typeof score);
@@ -7,3 +7,41 @@ let score = 33
   
  let valueInNumber = Number(score)
   console.log(typeof valueInNumber );
+
+  console.log(valueInNumber);
+
+
+
+  // "33"  == 33
+  // "33dh"  =  Nan 
+  // true  = 1
+  // false = 0
+  // undefined =  Nan
+
+
+  let isLoggedIn = "hwv"
+
+  let deepak = Boolean(isLoggedIn)
+  console.log(deepak);
+  
+
+   
+  
+// hietsh = true  
+// null = flase 
+//
+
+
+let number = 33
+
+let stringnumber = String(number)
+console.log(stringnumber)
+console.log(typeof number);
+
+
+
+let onlystring = "ravi"
+
+
+let onystring = Number()
+
