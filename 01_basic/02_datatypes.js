b02cd796db44014e9d2879f075b0bc2818dc9bbc
Deepak7900 <147752684+Deepak7900@users.noreply.github.com>
2025-08-20 06:27:10 +0000

    container config done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  //  treat all js code as newer version //
+
+
+//alert("hello")  // we are using node.js not browswer 
+
+// code realiblity should br high 
+
+
+let names = "deepak"
+let age = 20
+let isloggedin = false
+
+// number =  2 to power 53
+//bigint usually in stock n big web
+//string  =  ""
+// booolean  =  true/false
+//null = standalone value n
+//  undeined = 
+// symbol  = uniqur  like figma
+
+
+//object 
+
+console.log(typeof undefined)
